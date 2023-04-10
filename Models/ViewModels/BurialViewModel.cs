0dba8ba6f97e9290c291591dc067483f1ca265a7
Michael Chase <mchase62@byu.edu>
2023-04-10 14:07:28 -0600

    Michael's Changes

---
+++ Models/ViewModels/BurialViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MummyNation_Team0113.Models.ViewModels
+{
+    public class BurialViewModel
+    {
+        public IQueryable<Burialmain> Burials { get; set; }
+
+    }
+}
