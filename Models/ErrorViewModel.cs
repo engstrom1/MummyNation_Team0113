bae38dbfaf0afa12adf968fda179f2d9707ee062
Emma Engstrom <engstrome48@gmail.com>
2023-04-10 10:19:10 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MummyNation_Team0113.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
