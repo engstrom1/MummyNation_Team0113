60b42e5ee75d41bcd74dd33218dbe8e33d0f69a7
Michael Chase <mchase62@byu.edu>
2023-04-13 12:10:24 -0600

    Added CRUD functionality for Mummy Records

--- Models/IMummyNation_Team0113Repository.cs
+++ Models/IMummyNation_Team0113Repository.cs
@@ -8,5 +8,11 @@ namespace MummyNation_Team0113.Models
     public interface IMummyNation_Team0113Repository
     {
         IQueryable<Burialmain> burialmain { get; }
+
+        void AddBurialmain(Burialmain burialmain);
+        void UpdateBurialmain(Burialmain burialmain);
+        void DeleteBurialmain(long id);
+        Burialmain GetBurialmainById(long id);
+        MummyContext GetDbContext();
     }
 }
